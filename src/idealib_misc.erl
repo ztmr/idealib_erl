776fc63678a468eee0f00027ee5db277537ce344
Tomas Morstein <tmr&idea.cz>
2013-05-14 22:24:31 +0200

    UUIDv4 string generator;

--- src/idealib_misc.erl
+++ src/idealib_misc.erl
@@ -7,7 +7,17 @@
 %%
 
 -module (idealib_misc).
--export ([post_init/2, post_init/3, post_init_internal/3]).
+-export ([uuid4str/0, post_init/2, post_init/3, post_init_internal/3]).
+
+%% @doc UUIDv4 string generator.
+uuidv4str () ->
+  <<U0:32, U1:16, _:4, U2:12, _:2, U3:30, U4:32>> =
+    crypto:rand_bytes (16),
+  <<Ux0:32, Ux1:16, Ux2:16, Ux3:16, Ux4:48>> =
+    <<U0:32, U1:16, 4:4, U2:12, 2#10:2, U3:30, U4:32>>,
+  lists:flatten (io_lib:format (
+    "~8.16.0b-~4.16.0b-~4.16.0b-~4.16.0b-~12.16.0b",
+    [Ux0, Ux1, Ux2, Ux3, Ux4])).
 
 %% @doc Wait for the `App' to start and run `Fun' function finally.
 post_init (App, Fun) ->
