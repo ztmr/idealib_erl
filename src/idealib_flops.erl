ded7c66bd519b891dd574002ed9f50091af15858
Tomas Morstein <tmr@idea.cz>
2014-03-14 11:57:16 +0100

    add idealib_flops:round/1 and /2
    Unlike the
      erlang:round :: float -> integer
    this one is
      idealib_flops:round :: float -> float
    with optional variable precision.

--- src/idealib_flops.erl
+++ src/idealib_flops.erl
@@ -1,7 +1,8 @@
 -module (idealib_flops).
 -export ([
   add/2, substract/2,
-  integral/1, fractional/1
+  integral/1, fractional/1,
+  round/1, round/2
 ]).
 
 -define (FP_PRECISION, 1000000000000000).
@@ -21,6 +22,20 @@ integral (X) when is_float (X) -> erlang:trunc (X).
 fractional (X) when is_float (X) ->
   substract (X, integral (X)).
 
+-spec round (X::float ()) -> float ().
+%% @doc Round a float->float.
+%% Unlike the erlang:round/1, we don't return integer, but float.
+round (X) when is_float (X) ->
+    erlang:round (X) + 0.0.
+
+-spec round (X::float (), Precision::non_neg_integer ()) -> float ().
+%% @doc Round a float->float with specified precision.
+%% Unlike the erlang:round/1, we don't return integer, but float.
+round (X, 0) when is_float (X) -> % not necessary, but probably faster
+    ?MODULE:round (X);
+round (X, N) when is_float (X), is_integer (N), N >= 0 ->
+    Shift = math:pow (10, N),
+    ?MODULE:round (X * Shift) / Shift.
 
 %% EUnit Tests
 -ifdef (TEST).
@@ -29,11 +44,23 @@ fractional (X) when is_float (X) ->
 common_test () ->
 
   %% Basic
-  Frac = 0.123, Int = 678, F = add (Int, Frac),
+  Frac = 0.123, Frac2 = 0.678, Int = 678, F = add (Int, Frac),
   ?assertEqual (F, add (Int, Frac)),
   ?assertEqual (Int, integral (F)),
   ?assertEqual (Frac, fractional (F)),
   ?assertEqual (F, add (integral (F), fractional (F))),
+  ?assertEqual (0.0, ?MODULE:round (Frac)),
+  ?assertEqual (0.12, ?MODULE:round (Frac, 2)),
+  ?assertEqual (0.123000, ?MODULE:round (Frac, 6)),
+  ?assertEqual (-0.0, ?MODULE:round (-Frac)),
+  ?assertEqual (-0.12, ?MODULE:round (-Frac, 2)),
+  ?assertEqual (-0.123000, ?MODULE:round (-Frac, 6)),
+  ?assertEqual (1.0, ?MODULE:round (Frac2)),
+  ?assertEqual (0.68, ?MODULE:round (Frac2, 2)),
+  ?assertEqual (0.678000, ?MODULE:round (Frac2, 6)),
+  ?assertEqual (-1.0, ?MODULE:round (-Frac2)),
+  ?assertEqual (-0.68, ?MODULE:round (-Frac2, 2)),
+  ?assertEqual (-0.678000, ?MODULE:round (-Frac2, 6)),
 
   ok.
 
